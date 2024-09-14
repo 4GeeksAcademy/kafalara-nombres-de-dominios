ae4a3b78543927186a647ad18517ad4821055f7c
Kafalara <karen.larrosa@gmail.com>
2024-09-14 00:21:25 +0000

    proyecto entregado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+// window.onload = function() {
+function domainnames() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        let indexname = `${pronoun}${adj}${noun}.com`;
+        console.log(indexname);
+      }
+    }
+  }
+
+}
+domainnames();
